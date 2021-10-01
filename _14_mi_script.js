56b6e5c3783cf68c9f3f062dea8cc9d8c72c01fc
Jesus <jesusperezbec@hotmail.com>
2021-10-01 14:15:00 -0500

    Repaso hasta el archivo -18

--- _14_mi_script.js
+++ _14_mi_script.js
@@ -1,10 +1,10 @@
 /*
 Nuevos métodos para identificar elementos
 querySelector()
-Retorna el primer elemento que cumple con las caracteristicas de los parametros
+Retorna el primer elemento que cumple con las características de los parámetros
 
 querySelectorAll()
-Retorna todos los elementos que cumplen con las caracteristicas de los parametros,
+Retorna todos los elementos que cumplen con las características de los parámetros,
 se regresa un array con los elementos en el mismo orden con el que se encuentran
 en el documento (página)
 
@@ -12,19 +12,20 @@ entre los parentesis van los selectores css3 que queramos
 */
 
 function Ejecuta(){
-	//Devuelve el primer elemento de la pagina web que coincide con el selector(parametro)
+	//Devuelve el primer elemento de la pagina web que coincide con el selector (parámetro)
 	//document.querySelector(".import").onclick = Saludo;
 
-	//Seleccionando un elemento con pseudoclases de css(ultimo hijo p dentro de un contenedor de id=principal)
+	//Seleccionando un elemento con pseudoclases de css (último hijo <p> dentro de un contenedor con id=principal)
 	//document.querySelector("#principal p:last-child").onclick = Saludo;
 	
 	//Esto apunta a un elemento en especial por indice, no se requiere guardar en una variable.
 	//document.querySelectorAll("#principal p")[3].onclick = Saludo;
 
-	//Guardando todos los elementos que cumplan con determinada caracteristica (p dentro de #principal y span dentro de la web)
+	//Guardando todos los elementos que cumplan con determinada caracteristica (<p> dentro de #principal y span dentro de la web)
+	//Regresa un array
 	var elementos = document.querySelectorAll("#principal p, span");
 	
-	//Esto selecciona a todos mediante un for
+	//Recorre array y asigna función
 	for(var i = 0; i < elementos.length; i++){
 			elementos[i].onclick = Saludo;
 	}
--- _19_funcionalidad_barra.js
+++ _19_funcionalidad_barra.js
@@ -18,7 +18,10 @@ function comenzar(){
 
 //Creando funcion clicando
 /*El objeto video acepta las siguientes propiedades:
-Duration(duracion de video en segundos), currentTime(tiempo actual en segundos), Paused(regresa true o false), Ended(video terminado, regresa true o false)
+- duration (duracion de video en segundos)
+- currentTime (tiempo actual en segundos)
+- paused (regresa true o false)
+- ended (video terminado, regresa true o false)
 Metodos: play(), Pause().
 */
 function clicando(){
@@ -32,19 +35,18 @@ function clicando(){
 		reproducir.innerHTML = "Pause";
 
 		//Llamando funcion
-		//Con la funcion setInterval se llama a una funcion cada cierto tiempo
-		//como primer parametro lleva el nombre de la funcion, como segundo lleva 
-		//cada cuanto debe llmarse en milisegundos.
-		bucle = setInterval(estado, 5); //Entre mas pequeño sea el segundo parametro la barra crece mejor
+		//Con la funcion setInterval se llama a una funcion cada cierto tiempo, incluso se sigue llamando si el video está pausado
+		//como primer parámetro lleva el nombre de la funcion, como segundo lleva 
+		//cada cuanto debe llamarse en milisegundos.
+		bucle = setInterval(estado, 5); //Entre mas pequeño sea el segundo parámetro la barra crece mejor
 	}
 }
 
 /*reflejar el estado del video*/
 //Se llama cuando el video se esté reproduciendo.
 function estado(){
-	//Verificar cuanto va del video
+	//comprobando que el video no haya terminado
 	if(miVideo.ended == false){
-		//comprobando que no haya terminado
 
 		//Valor sacado con una regla de tres.
 		var total = parseInt(miVideo.currentTime * maximo / miVideo.duration);
@@ -57,11 +59,11 @@ function estado(){
 }
 
 //Funcion para adelantar o retrasar el video
-//PageX regresa donde se encuantra el raton en el eje de las x
-//Offsetleft- regresa cuanto hay del margen izquierdo hasta la barra
+//PageX regresa donde se encuentra el ratón en el eje de las x (horizontal)
+//Offsetleft - regresa cuanto hay de espacio desde el margen izquierdo(borde de página) hasta el inicio de la barra
 function cambiar(posicion){
 	if((miVideo.paused == false) && (miVideo.ended == false)){
-		//Sacar posición del raton en ej eje de x (desde borde izquierdo hasta el puntero)
+		//Sacar posición del raton en el eje de x (desde borde izquierdo hasta el puntero)
 		var ratonX = posicion.pageX - barra.offsetLeft; 
 		//offsetLeft, regresa la cantidad de px entre el borde izquierdo y el elemento al que se le aplica
 
